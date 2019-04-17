439c13d8c2e66b822bc4bbb9f1597dc7d188f71a
dogballlee <zeus.1989@163.com>
2019-04-17 13:42:13 +0800

    version_1.0

---
+++ 文字转拼音.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+
+import pandas as pd
+import numpy as np
+from xpinyin import Pinyin
+
+#把需要转拼音的列名称改为'汉字'
+path = r'C:\Users\lilinyang\Desktop\pinyin.csv'
+
+p = Pinyin()
+a = pd.read_csv(path)
+l = a['汉字']
+pinyin_list = []
+for i in l:
+    t = p.get_pinyin(i,'')
+    pinyin_list.append(t)
+
+result = pd.Series(pinyin_list)
+print(result)
+
